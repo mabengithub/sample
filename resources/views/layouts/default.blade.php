33f168d4138a235653993f01b66a09e58ae8e6fb
maben <mabenchn@gmail.com>
2019-01-20 08:43:52 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,26 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>@yield('title','Sample')</title>
+
+ <!DOCTYPE html>
+
+ <html>
+  
+   <head>
+   
+      <title>@yield('title','Sample App')</title>
+      
+      <link rel="stylesheet" href="/css/app.css">
+  
   </head>
+  
   <body>
-    @yield('content')
+    @include('layouts._header') 
+	
+   <div class="container">
+  
+	 @yield('content')
+ 	 @include('layouts._footer')
+
+   </div>
+
   </body>
+
 </html>
