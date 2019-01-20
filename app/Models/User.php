faf31c49949ec9e4e4ac5fc49a5b62ff1d867205
maben <mabenchn@gmail.com>
2019-01-20 12:53:53 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,4 +28,15 @@ class User extends Authenticatable
     ];
 
     protected $table = 'users';
+
+    //Gravatar用来提供个人头像支持 ,"全球通用头像"
+    public function gravatar($size = '100')
+    {
+	$hash = md5(strtolower(trim($this->attributes['email'])));
+
+	return "http://www.gravatar.com/avatar/$hash?s=$size";
+    
+    }
+
+
 }
